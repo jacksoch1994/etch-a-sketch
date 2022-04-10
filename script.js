350265c6ddb6e7c73d1adb4f0137285fcd9a13ed
Christopher H Jackson <jacksoch1994@gmail.com>
2022-04-10 10:52:46 -0500

    Added user prompt to get side length. Introduced gradual color change.
    Reset button now prompts user for the amount of pixels desired per side
    of the canvas. Mousing-Over a pixel now applies a CSS class depending on the
    classes already in the class list. Each subsequent class makes the pixel
    darker until completely black.

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-console.log("Hello");
-
-let rows = 50;
-let columns = 50;
-
 const canvas = document.querySelector("#etch-a-sketch");
 
-for (let k = 0; k < columns; k++) {
+function createPixels(length = 16) {
+    for (let k = 0; k < length; k++) {
 
-    let column = document.createElement('div');
-    column.classList.add("column");
-
-    for (let i = 0; i < rows; i++) {
-        let pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        column.appendChild(pixel);
+        let column = document.createElement('div');
+        column.classList.add("column");
+    
+        for (let i = 0; i < length; i++) {
+            let pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            column.appendChild(pixel);
+        }
+    
+        canvas.appendChild(column);
+    
     }
-
-    canvas.appendChild(column);
-
+    addListeners();
 }
-
-addListeners();
 
 function addListeners() {
     let pixels = document.querySelectorAll(".pixel");
@@ -32,12 +28,36 @@ function addListeners() {
 }
 
 function changeColor(e) {
-    e.currentTarget.classList.add("active");
+    let classes = e.currentTarget.classList;
+    if (classes.contains('active-3')) {
+        return;
+    } else if (classes.contains('active-2')) {
+        classes.add("active-3");
+    } else if (classes.contains('active-1')) {
+        classes.add("active-2");
+    } else {
+        classes.add("active-1");
+    }
 }
 
 function reset() {
-    let pixels = document.querySelectorAll(".pixel");
-    pixels.forEach((pixel) => {
-        pixel.setAttribute('class', 'pixel');
+    let columns = document.querySelectorAll(".column");
+    columns.forEach((col) => {
+        col.remove();
     });
+    createPixels(promptUserForLength());
+
 }
+
+function promptUserForLength() {
+    let length = prompt("Enter the number of pixels per side (MIN: 2, MAX: 100): ", 16);
+    if (parseInt(length)) {
+        if (length < 2) length = 2;
+        if (length > 100) length = 100;
+    } else {
+        alert("Could not parse input. Please input an integer value.")
+    }
+    return length;
+}
+
+createPixels()
